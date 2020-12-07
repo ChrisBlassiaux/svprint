74f007422f0ddd811b49b1d7f352f92c817f4ba1
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-08 00:01:38 +0100

    feat(home): init css introducing

--- index.php
+++ index.php
@@ -3,13 +3,107 @@ $title = 'SVprint - Vente de matériel de bureau';
 require 'layouts/header.php'; 
 ?>
 
-  <div class="container">
-    <div class="indroducing">
-      <img src="images/svg_1.png" alt="Image représentant le logotypes de l'entreprise">
-      <h2 class="title">SV Print :  qui sommes nous ? </h2>
-      <p>Située dans le nord de la France, à la Nouvelle Forge d’Anzin, notre entreprise est spécialisée dans la vente de consommables, matériaux fournitures, et conseils pour les entreprises.</p>
+  <div class="introducing_step">
+    <div class="number">
+      <span class="bold">1</span>
+    </div>
+    <div class="line line-1"></div>
+    <div class="number">
+      <span class="bold">2</span>
+    </div>
+    <div class="line line-2"></div>
+    <div class="number">
+      <span class="bold">3</span>
+    </div>
+    <div class="line line-3"></div>
+    <div class="number">
+      <span class="bold">4</span>
+    </div>
+    <div class="line line-4"></div>
+    <div class="number">
+      <span class="bold">5</span>
     </div>
   </div>
+
+  <div class="introducing">
+    <div class="container-small section">
+      <div class="text-center">
+        <img src="images/svg_1.png" alt="Image représentant le logotypes de l'entreprise">
+      </div>
+      <div>
+        <h2 class="title">SV Print :  qui sommes nous ? </h2>
+        <p>Située dans le nord de la France, à la Nouvelle Forge d’Anzin, notre entreprise est spécialisée dans la vente de consommables, matériaux fournitures, et conseils pour les entreprises.</p>
+      </div>
+    </div>
+    <div class="container-small">
+      <div class="text-center">
+        <h2 class="title">Pourquoi SV Print ? </h2>
+        <p>SV Print vous propose un accompagnement personnalisé en s’adaptant à vos problèmatiques.</p>
+      </div>
+      <div class="container-small section section-small">
+        <div class="img">
+          <img src="images/svg_1.png" alt="Image représentant le logotypes de l'entreprise">
+        </div>
+        <div>
+          <h3 class="subtitle">Equipe d’experts</h3>
+          <p>Nous sommes une équipe d’experts à votre écoute avec plus de 10 ans d’expériences.</p>
+        </div>
+      </div>
+      <div class="container-small section section-small">
+        <div class="img">
+          <img src="images/svg_1.png" alt="Image représentant le logotypes de l'entreprise">
+        </div>
+        <div>
+          <h3 class="subtitle">Un interlocuteur unique</h3>
+          <p>Nous vous proposons un interloculeur unique qui permettra un suivant constant de votre situation. </p>
+        </div>
+      </div>
+      <div class="container-small section section-small">
+        <div class="img">
+          <img src="images/svg_1.png" alt="Image représentant le logotypes de l'entreprise">
+        </div>
+        <div>
+          <h3 class="subtitle">Un accompagnement</h3>
+          <p>L’installation, l’entretien de vos appareils et le réapprovisionnement de consommables.</p>
+        </div>
+      </div>
+      <div class="container-small section section-small">
+        <div class="img">
+          <img src="images/svg_1.png" alt="Image représentant le logotypes de l'entreprise">
+        </div>
+        <div>
+          <h3 class="subtitle">Un guide</h3>
+          <p>Nous vous accompagnons lors du choix de vos équipements. Nous vous guidons lors des diagnostiques de vos besoins.
+          Choississez-nous, gagnez du temps, travaillez sereinement.</p>
+        </div>
+      </div>
+    </div>
+    <div class="container-small section">
+      <div class="text-center">
+        <img src="images/svg_1.png" alt="Image représentant le logotypes de l'entreprise">
+      </div>
+      <div>
+        <h2 class="title">SV Print et l’Écologie</h2>
+        <p>Sur votre demande, nous mettons en place la 
+          reprise de vos cartouches vides à nos frais, 
+          pour un traitement écologique. 
+          Plus d'encombrement et une bonne action
+          pour la planète (cartouches recyclables).
+        </p>
+      </div>
+    </div>
+    <div class="container-small section">
+      <div class="text-center">
+        <img src="images/svg_1.png" alt="Image représentant le logotypes de l'entreprise">
+      </div>
+      <div>
+        <h2 class="title">Nos services</h2>
+        <p>Nous offrons des cartouches compatibles, remanufacturée haut de gamme.</p>
+      </div>
+    </div>
+  </div>
+
+ 
 
 <?php require 'layouts/footer.php'; ?> 
 
--- layouts/header.php
+++ layouts/header.php
@@ -56,17 +56,21 @@
         </div>
       </div>
       <div class="container-jumbotron">
-        <p class="presentation_keys"><span class="fa fa-print fa-2x mr-3"></span>Vente de consommable</p>
-        <p class="presentation_keys"><span class="fa fa-question fa-2x mr-3"></span>Vente de consommable</p>
-        <div class="jumbotron">
-          <div class="presentation">
-            <div class="container">
-              <a href="service.php" class="btn btn-primary">Des milliers de références<i class="fa fa-angle-right"></i></a>
-            </div> 
-            <div class="text">
-              <div class="container bold text-center">
-                Vente de consommable sur des milliers de références, matériaux fournitures.
-                Et conseils pour les entreprises.
+        <div class="presentation-keys">
+          <p class="presentation_key"><span class="fa fa-print fa-2x mr-3"></span>Vente de consommable</p>
+          <p class="presentation_key"><span class="fa fa-question fa-2x mr-3"></span>Aides et conseils pour les entreprises</p>
+        </div>
+        <div class="container-small">
+          <div class="jumbotron">
+            <div class="presentation">
+              <div class="container text-center">
+                <a href="service.php" class="btn btn-primary">Des milliers de références<i class="fa fa-angle-right"></i></a>
+              </div> 
+              <div class="text">
+                <div class="container bold text-center">
+                  Vente de consommable sur des milliers de références, matériaux fournitures.
+                  Et conseils pour les entreprises.
+                </div>
               </div>
             </div>
           </div>
