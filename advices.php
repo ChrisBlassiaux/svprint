5aa5ddc7965fb391d65f552b8b8db3b0e0c2ce86
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-12-07 19:21:05 +0100

    added advices php file; added html structure to advices.php

---
+++ advices.php
@@ -0,0 +1,94 @@
+<?php 
+$title = 'SVprint - Conseils et astuces';
+require 'layouts/header.php'; ?>
+
+<main>
+<section>
+
+</section>
+
+<section>
+	<h2>Conseils et Astuces</h2>
+	<div class="conseil">
+		<div>
+			<h3>Astuce 1</h3>
+			<p>Achetez du matériel qui correspond à vos besoin</p>
+		</div>
+		<div>
+		
+		</div>
+	</div>
+	<div class="conseil">
+		<div>
+			<h3>Astuce 2</h3>
+			<p>Pensez à télécharger les pilotes de votre matériel, et à les mettres à jours pour une utilisation optimale de vos équipements</p>
+		</div>
+		<div>
+		
+		</div>
+	</div>
+	<div class="conseil">
+		<div>
+			<h3>Astuce 3</h3>
+			<p>N'oubliez pas d'activer toutes les options de vos équipements, pour bénéficier de 100% de ce qu'ils offrent</p>
+		</div>
+		<div>
+		
+		</div>
+	</div>
+	<div class="conseil">
+		<div>
+			<h3>Astuce 4</h3>
+			<p>
+				Nettoyage de votre imprimante :
+				<ul>
+					<li>Pour les imprimantes à jet d'encre, utilisez une bombe à gaz sec pour déloger les résidus.</li>
+					<li>Pour les imprimantes laser, utilisez un feuillet de nettoyage (une feuille magnétique qui attirera les résidus)</li>
+				</ul> 
+			</p>
+		</div>
+		<div>
+		
+		</div>
+	</div>
+	<div class="conseil">
+		<div>
+			<h3>Astuce 5</h3>
+			<p>Toujours utiliser un papier adapté à la machine, afin de miniser les risques de bourrages papier ou des problèmes internes</p>
+		</div>
+		<div>
+		
+		</div>
+	</div>
+	<div class="conseil">
+		<div>
+			<h3>Astuce 6</h3>
+			<p>Privilégiez les cartouches XL quand possible</p>
+		</div>
+		<div>
+		
+		</div>
+	</div>
+	<div class="conseil">
+		<div>
+			<h3>Astuce 7</h3>
+			<p>Utiliser des cartouches d'encres, remanufacturées haute qualité, avec garantie</p>
+		</div>
+		<div>
+		
+		</div>
+	</div>
+	<div class="conseil">
+		<div>
+			<h3>Astuce 8</h3>
+			<p>Rappelez-vous de changer les pièces d'usures dès que nécessaires, n'attendez pas le derniers moment, cela pourrait provoquer des pannes plus sérieuses</p>
+		</div>
+		<div>
+		
+		</div>
+	</div>
+</section>
+
+</main>
+
+<?php require 'layouts/footer.php'; ?>
