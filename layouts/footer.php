16ea76af736449b32909d529409fcda68181b44f
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-08 01:01:27 +0100

    feat(home): init css footer section

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,4 +1,43 @@
-    <a href="#" title="Revenir en haut de page">
+   
+     <footer class="footer">
+      <div class="container">
+        <div class="lists">
+          <div class="list">
+            <h4 class="title-list">Adresse</h4>
+            <ul>
+              <li>80 Avenue Roland Moreno</li>
+              <li>59410 Anzin</li>
+            </ul>
+          </div>
+          <div class="list">
+            <h4 class="title-list">Nos réseaux</h4>
+            <ul>
+              <li><a href="#">Instagram</a></li>
+              <li><a href="#">Linkedin</a></li>
+              <li><a href="#">Facebook</a></li>
+            </ul>
+          </div>
+          <div class="list">
+            <h4 class="title-list">Rubriques</h4>
+            <ul>
+              <li><a href="#">Accueil</a></li>
+              <li><a href="#">Nos services</a></li>
+              <li><a href="#">Conseils</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+          <div class="list">
+            <h4 class="title-list">Mentions</h4>
+            <ul>
+              <li><a href="#">Mentions légales</a></li>
+              <li><a href="#">Politique de confidencialités</a></li>
+            </ul>
+          </div>
+         </div>
+        </div>
+    </footer>
+   
+   <a href="#" title="Revenir en haut de page">
       <span class="back-to-top">
         <span class="fa fa-angle-up fa-2x"></span>
       </span>
