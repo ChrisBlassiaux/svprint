5a9b89307e245238069ef97929cbd277e36640c5
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-10 23:34:36 +0100

    feat(home): animation svg

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,6 @@ let navbar = document.querySelector('.navbar');
 let burger = document.querySelector('.contain-burger');
 let nav = document.querySelector('.nav');
 
-let dropDownEvents = document.querySelector('.events-cities');
 let eventsTitle = document.querySelector('.section-title-events');
 
 let backToTop = document.querySelector('.back-to-top');
@@ -51,7 +50,6 @@ function scrollFunction() {
         element.style.padding = "20px 15px";
       });
     }
-    dropDownEvents.style.top = "55px";
     navbar.style.borderBottom = "4px solid #DBEDF8";
   } else {
     backToTop.style.display = "none";
@@ -64,7 +62,30 @@ function scrollFunction() {
         element.style.padding = "42px 15px";
       });
     }
-    dropDownEvents.style.top = "80px";
     navbar.style.borderBottom = "none";
   }
 }
+
+
+
+window.onscroll = function() {scroll2Function()};
+
+let svg1 = document.querySelector('.wrapper-1 svg');
+let svg2 = document.querySelector('.wrapper-2 svg');
+let svg3 = document.querySelector('.wrapper-3 svg');
+let svg4 = document.querySelector('.wrapper-4 svg');
+let svg5 = document.querySelector('.wrapper-5 svg');
+let svg6 = document.querySelector('.wrapper-6 svg');
+let svg7 = document.querySelector('.wrapper-7 svg');
+
+function scroll2Function() {
+  document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000 ? svg1.classList.add('active') : svg1.classList.remove('active');
+  document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200 ? svg2.classList.add('active') : svg2.classList.remove('active');
+  document.body.scrollTop > 1400 || document.documentElement.scrollTop > 1400 ? svg3.classList.add('active') : svg3.classList.remove('active');
+  document.body.scrollTop > 1600 || document.documentElement.scrollTop > 1600 ? svg4.classList.add('active') : svg4.classList.remove('active');
+  document.body.scrollTop > 1900 || document.documentElement.scrollTop > 1900 ? svg5.classList.add('active') : svg5.classList.remove('active');
+  document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000 ? svg6.classList.add('active') : svg6.classList.remove('active');
+  document.body.scrollTop > 2200 || document.documentElement.scrollTop > 2200 ? svg7.classList.add('active') : svg7.classList.remove('active');
+}
+
+
